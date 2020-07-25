8418d4a58768f87fb949aed3397252c67cbcaa50
Mostafa_Tarek <mthassan@ejada.com>
2020-07-26 01:08:20 +0300

    hel

--- machine-learning-case-studies/Lesson1-population-segmentation/population-segmentation/5-reduce-dimnsion-PCA.py
+++ machine-learning-case-studies/Lesson1-population-segmentation/population-segmentation/5-reduce-dimnsion-PCA.py
@@ -72,4 +72,86 @@ import mxnet as mx
 pca_model_params = mx.ndarray.load('model_algo-1')
 
 # what are the params
+""""
+mean: The mean that was subtracted from a component in order to center it.
+v: The makeup of the principal components; (same as ‘components_’ in an sklearn PCA model).
+s: The singular values of the components for the PCA transformation. This does not exactly give the % variance from the original feature space, but can give the % variance from the projected feature space.
+""""
 print(pca_model_params)
+
+
+# get selected params
+s=pd.DataFrame(pca_model_params['s'].asnumpy())
+
+v=pd.DataFrame(pca_model_params['v'].asnumpy())
+v
+
+
+# Calculate the explained variance for the top n principal components
+# you may assume you have access to the global var N_COMPONENTS
+def explained_variance(s, n_top_components):
+    '''Calculates the approx. data variance that n_top_components captures.
+       :param s: A dataframe of singular values for top components; 
+           the top value is in the last row.
+       :param n_top_components: An integer, the number of top components to use.
+       :return: The expected data variance covered by the n_top_components.'''
+    
+    # get start idx just like above
+    start_idx = N_COMPONENTS - n_top_components
+    
+    #calculate variance
+    upper = np.sum(np.square(s.iloc[start_idx:,0]))
+    lower = np.sum(np.square(s.iloc[:,0]))
+    
+    return upper/lower 
+    # your code here
+    
+    pass
+
+# test cell
+n_top_components = 7 # select a value for the number of top components
+
+# calculate the explained variance
+exp_variance = explained_variance(s, n_top_components)
+print('Explained variance: ', exp_variance)
+
+""""
+We can now examine the makeup of each PCA component based on the weightings of the original features 
+hat are included in the component. The following code shows the feature-level makeup of the first component.
+
+Note that the components are again ordered from smallest to largest and 
+so I am getting the correct rows by calling N_COMPONENTS-1 to get the top, 1, component.
+""""
+
+import seaborn as sns
+
+def display_component(v, features_list, component_num, n_weights=10):
+    
+    # get index of component (last row - component_num)
+    row_idx = N_COMPONENTS-component_num
+
+    # get the list of weights from a row in v, dataframe
+    v_1_row = v.iloc[:, row_idx]
+    v_1 = np.squeeze(v_1_row.values)
+    # match weights to features in counties_scaled dataframe, using list comporehension
+    comps = pd.DataFrame(list(zip(v_1, features_list)), 
+                         columns=['weights', 'features'])
+    # we'll want to sort by the largest n_weights
+    # weights can be neg/pos and we'll sort by magnitude
+    comps['abs_weights']=comps['weights'].apply(lambda x: np.abs(x))
+    sorted_weight_data = comps.sort_values('abs_weights', ascending=False).head(n_weights)
+    # display using seaborn
+    ax=plt.subplots(figsize=(10,6))
+    ax=sns.barplot(data=sorted_weight_data, 
+                   x="weights", 
+                   y="features", 
+                   palette="Blues_d")
+    ax.set_title("PCA Component Makeup, Component #" + str(component_num))
+    plt.show()
+
+# Deploying the PCA Model
+    
+%%time
+# this takes a little while, around 7mins
+pca_predictor = pca_SM.deploy(initial_instance_count=1, 
+                              instance_type='ml.t2.medium')    
